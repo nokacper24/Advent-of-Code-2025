e8f77216968e64e4f1123041c20b7099f0b3fa26
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2025-12-02 21:26:03 +0100

    Solve d2 p1

---
+++ 02_01/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string content = await File.ReadAllTextAsync("input");
+var ranges = content.Split(',');
+
+
+long total = 0;
+foreach (var range in ranges)
+{
+    var vals = range.Split('-');
+    var min = long.Parse(vals[0]);
+    var max = long.Parse(vals[1]);
+    for (long i = min; i <= max; i++)
+    {
+        if (isInvalid(i.ToString()))
+        {
+            total += i;
+        }
+    }
+
+}
+
+Console.WriteLine(total);
+
+
+bool isInvalid(string digits)
+{
+    if (digits.Length % 2 != 0)
+    {
+        return false;
+    }
+    var half1 = digits.Substring(0, digits.Length / 2);
+    var half2 = digits.Substring(digits.Length / 2, digits.Length / 2);
+    return half1 == half2;
+}
