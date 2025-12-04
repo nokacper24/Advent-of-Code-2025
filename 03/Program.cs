e343b6872ed6fc27bd8e6d7579033ee80e9ef55f
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2025-12-04 17:37:47 +0100

    Solve d3 p2

--- 03/Program.cs
+++ 03/Program.cs
@@ -1,22 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
 Console.WriteLine("Hello, World!");
 
-var total = 0;
+long total = 0;
 await foreach (var bank in File.ReadLinesAsync("input"))
 {
-    var joltage = getHighestJoltage(bank);
+    var joltage = getHighestJoltage2(bank,12);
     Console.WriteLine(joltage);
+    // break;
 
     total += joltage;
 }
-Console.WriteLine(total);
+Console.WriteLine($"Total: {total}");
 
 
 int getHighestJoltage(string bank)
 {
     var firstDigitIndex = getIndexOfHighestDigit(bank.Substring(0, bank.Length - 1));
-    var secondDigitIndex = getIndexOfHighestDigit(bank.Substring(firstDigitIndex+1)) + firstDigitIndex + 1;
+    var secondDigitIndex = getIndexOfHighestDigit(bank.Substring(firstDigitIndex + 1)) + firstDigitIndex + 1;
     return int.Parse($"{bank[firstDigitIndex]}{bank[secondDigitIndex]}");
+}
+
+long getHighestJoltage2(string bank, int numOfDigits)
+{
+    StringBuilder sb = new();
+    var leftPointer = 0;
+    var rightPointer = bank.Length - numOfDigits+1;
+    for (int i = 0; i < numOfDigits; i++)
+    {
+        // Console.WriteLine($"{leftPointer} - {rightPointer}");
+        var searchableSubstring = bank[leftPointer..rightPointer];
+        // Console.WriteLine($"sarching in:{searchableSubstring}");
+        var next = getIndexOfHighestDigit(searchableSubstring) + leftPointer;
+        // Console.WriteLine($"next:{next} ({bank[next]})");
+
+        leftPointer = next+1;
+        rightPointer++;
+        sb.Append(bank[next]);
+    }
+    return long.Parse(sb.ToString());
 }
 
 int getIndexOfHighestDigit(string text)
