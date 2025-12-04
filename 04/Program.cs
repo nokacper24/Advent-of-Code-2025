5930802fda25006da425efa27e69582138e4f7a0
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2025-12-04 18:24:57 +0100

    Solve d4 p1

---
+++ 04/Program.cs
@@ -0,0 +1,83 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+char[][] diagram = [];
+
+await foreach (var line in File.ReadLinesAsync("input"))
+{
+    diagram = [.. diagram, line.ToCharArray()];
+}
+
+solvePart1();
+
+void solvePart1()
+{
+    int total = 0;
+    for (int i = 0; i < diagram.Length; i++)
+    {
+        for (int j = 0; j < diagram[i].Length; j++)
+        {
+            if (diagram[i][j] == '@')
+            {
+                var neighbours = countNeighbours(diagram, i, j);
+                Console.WriteLine($"{i} {j} -> {neighbours}");
+                if (neighbours < 4)
+                {
+                    total++;
+                }
+            }
+        }
+
+    }
+    Console.WriteLine($"Total: {total}");
+}
+
+static int countNeighbours(char[][] diagram, int x, int y)
+{
+    int neighbourCount = 0;
+    for (int dx = -1; dx <= 1; dx++)
+    {
+        for (int dy = -1; dy <= 1; dy++)
+        {
+            if (dx == 0 && dy == 0)
+            {
+                // Console.WriteLine("skipping 0 0");
+                continue;
+            }
+
+            int checkX = x + dx;
+            int checkY = y + dy;
+            // Console.WriteLine($"checking {checkX} {checkY}");
+            if (checkX < 0 || checkY < 0)
+            {
+                // Console.WriteLine("too low, out of bounds");
+                continue;
+            }
+
+            if (checkX >= diagram.Length || checkY >= diagram[checkX].Length)
+            {
+                // Console.WriteLine("too high, out of bounds");
+                continue;
+            }
+
+            if (diagram[checkX][checkY] == '@')
+            {
+                neighbourCount++;
+            }
+        }
+    }
+    return neighbourCount;
+}
+
+
+static void print(char[][] diagram)
+{
+    for (int i = 0; i < diagram.Length; i++)
+    {
+        for (int j = 0; j < diagram[i].Length; j++)
+        {
+            Console.Write(diagram[i][j]);
+        }
+        Console.Write("\n");
+    }
+}
