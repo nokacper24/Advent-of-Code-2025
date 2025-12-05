0b8b6515addebc9718ad26b85649f2e0af8c41f1
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2025-12-05 17:51:46 +0100

    Solve d5 p2

--- 05/Program.cs
+++ 05/Program.cs
@@ -25,7 +25,7 @@ await foreach (var line in File.ReadLinesAsync("input"))
     }
 }
 
-solve1();
+// solve1();
 void solve1()
 {
     long freshCount = 0;
@@ -37,4 +37,38 @@ void solve1()
         }
     }
     Console.WriteLine(freshCount);
+}
+
+solve2();
+void solve2()
+{
+    long total = 0;
+    ranges.Sort((a, b) => a[0].CompareTo(b[0]));
+    long leftPointer = ranges[0][0];
+    long rightPointer = ranges[0][1];
+
+    for (int i = 1; i < ranges.Count; i++)
+    {
+        var range = ranges[i];
+        var start = range[0];
+        var end = range[1];
+
+        if (start > rightPointer + 1)
+        {
+            Console.WriteLine($"{leftPointer}-{rightPointer}");
+            total += rightPointer - leftPointer + 1;
+
+            leftPointer = start;
+            rightPointer = end;
+        }
+        else if (end > rightPointer)
+        {
+            rightPointer = end;
+        }
+    }
+    Console.WriteLine($"{leftPointer}-{rightPointer}");
+    total += rightPointer - leftPointer + 1;
+
+    Console.WriteLine(total);
+
 }
