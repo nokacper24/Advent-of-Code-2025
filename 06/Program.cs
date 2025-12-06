f4006da95dd373a1a4b772b126e5f3224acee677
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2025-12-06 12:55:32 +0100

    Solve d6 p2

--- 06/Program.cs
+++ 06/Program.cs
@@ -1,20 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
 using System.Text.RegularExpressions;
+using Microsoft.VisualBasic;
 
 Console.WriteLine("Hello, World!");
 
-string[][] worksheet = [];
 
-await foreach (var line in File.ReadLinesAsync("input"))
+await solve1();
+async Task solve1()
 {
-    Regex regex = new("[ ]{2,}", RegexOptions.None);
-    var normalizedLine = regex.Replace(line, " ").Trim();
-    worksheet = [.. worksheet, normalizedLine.Split(' ')];
-}
 
-solve1();
-void solve1()
-{
+    string[][] worksheet = [];
+
+    await foreach (var line in File.ReadLinesAsync("testinput"))
+    {
+        Regex regex = new("[ ]{2,}", RegexOptions.None);
+        var normalizedLine = regex.Replace(line, " ").Trim();
+        worksheet = [.. worksheet, normalizedLine.Split(' ')];
+    }
     long total = 0;
     for (int i = 0; i < worksheet[0].Length; i++)
     {
@@ -35,4 +38,76 @@ void solve1()
         }
     }
     Console.WriteLine(total);
+}
+
+
+await solve2();
+async Task solve2()
+{
+
+    Problem[] worksheet = [];
+
+    var lines = await File.ReadAllLinesAsync("input");
+    var height = lines.Length;
+
+    var problem = new Problem();
+
+    for (int x = 0; x < lines[0].Length; x++)
+    {
+        StringBuilder sb = new();
+        for (int y = 0; y < height; y++)
+        {
+            var c = lines[y][x];
+            Console.WriteLine($"Looking at [{c}]");
+
+            if (y == height - 1 && c != ' ')
+            {
+                if (problem.Values.Length != 0)
+                {
+                    worksheet = [.. worksheet, problem];
+                }
+                problem = new Problem
+                {
+                    Sign = c
+                };
+            }
+            else if (c != ' ')
+            {
+                sb.Append(c);
+            }
+        }
+        if (!string.IsNullOrEmpty(sb.ToString()))
+        {
+            problem.Values = [.. problem.Values, long.Parse(sb.ToString())];
+        }
+        sb.Clear();
+    }
+    worksheet = [.. worksheet, problem];
+    
+    long total = 0;
+    foreach (var p in worksheet)
+    {
+        Console.WriteLine(p);
+        if (p.Sign == '+')
+        {
+            total += p.Values.Aggregate((a, c) => a + c);
+        }
+        else
+        {
+            total += p.Values.Aggregate((a, c) => a * c);
+        }
+    }
+    Console.WriteLine(total);
+
+}
+
+record Problem
+{
+    public char Sign { get; set; }
+    public long[] Values { get; set; } = [];
+
+    public override string ToString()
+    {
+        return $"{{ Sign = {Sign}, Values = [{string.Join(", ", Values)}] }}";
+    }
 }
