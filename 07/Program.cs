a86e8a609924c7ff3cba3f731074e6f1b456f9c4
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2025-12-07 12:33:46 +0100

    Solve d7 p2

--- 07/Program.cs
+++ 07/Program.cs
@@ -14,10 +14,6 @@ async Task solve1()
         for (int j = 0; j < line.Length; j++)
         {
             var c = line[j];
-            if (c == 'S')
-            {
-                currentBeams[j] = true;
-            }
             switch (c)
             {
                 case 'S':
@@ -35,7 +31,7 @@ async Task solve1()
                             currentBeams[jRight] = true;
                         }
                         var jLeft = j - 1;
-                        if (jLeft > 0)
+                        if (jLeft >= 0)
                         {
                             currentBeams[jLeft] = true;
                         }
@@ -48,5 +44,60 @@ async Task solve1()
         }
     }
     Console.WriteLine(splitCount);
+
+}
+
+
+await solve2();
+async Task solve2()
+{
+    var input = await File.ReadAllLinesAsync("input");
+
+
+    long[] paths = new long[input[0].Length];
+    for (int i = 0; i < input.Length; i++)
+    {
+        int[] beamsToSwitchOff = [];
+        var line = input[i];
+        for (int j = 0; j < line.Length; j++)
+        {
+            var c = line[j];
+            switch (c)
+            {
+                case 'S':
+                    paths[j]++;
+                    break;
+                case '^':
+                    if (paths[j] > 0)
+                    {
+                        beamsToSwitchOff = [.. beamsToSwitchOff, j];
+
+                        var jRight = j + 1;
+                        if (jRight < line.Length)
+                        {
+                            paths[jRight] += paths[j];
+                        }
+                        var jLeft = j - 1;
+                        if (jLeft >= 0)
+                        {
+                            paths[jLeft] += paths[j];
+                        }
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        foreach (var b in beamsToSwitchOff)
+        {
+            paths[b] = 0;
+        }
+
+        // Console.WriteLine(string.Join(" ", paths.Select(x => x == 0 ? "   " : x.ToString().PadLeft(3))));
+
+    }
+
+    Console.WriteLine(paths.Aggregate((a, c) => a + c));
 
 }
